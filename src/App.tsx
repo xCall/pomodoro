86b1c6778d52feef817017f28aa32541e90fc5dc
xCall <carlosfaita99@gmail.com>
2022-12-08 22:47:29 -0300

    adicionado estilizacao de home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-      <GlobalStyle />
-      {" "}
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />{' '}
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
